1414a601ffdea6efe20ac7ac3b6740a95aaf9b35
beautyonly <beautytao@protonmail.com>
2018-04-10 16:41:16 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,63 @@
+FROM ubuntu:14.04
+MAINTAINER Eugene Beauty <bybeauby@gmail.com>
+
+# 保持最新
+RUN dpkg-divert --local --rename --add /sbin/initctl
+RUN ln -sf /bin/true /sbin/initctl
+
+# 让容器知道里没有tty
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update
+RUN apt-get -y upgrade
+
+# 基本要求
+RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
+
+# Wordpress 基本要求
+RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+
+# 修改mysql链接配置config
+RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+# nginx-配置-config
+RUN sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
+RUN sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf
+RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+
+# php-fpm config
+RUN sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
+RUN sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 100M/g" /etc/php5/fpm/php.ini
+RUN sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 100M/g" /etc/php5/fpm/php.ini
+RUN sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" /etc/php5/fpm/php-fpm.conf
+RUN sed -i -e "s/;catch_workers_output\s*=\s*yes/catch_workers_output = yes/g" /etc/php5/fpm/pool.d/www.conf
+RUN find /etc/php5/cli/conf.d/ -name "*.ini" -exec sed -i -re 's/^(\s*)#(.*)/\1;\2/g' {} \;
+
+# 添加nginx.conf配置容器中
+ADD ./nginx-site.conf /etc/nginx/sites-available/default
+
+# Supervisor Config
+RUN /usr/bin/easy_install supervisor
+RUN /usr/bin/easy_install supervisor-stdout
+ADD ./supervisord.conf /etc/supervisord.conf
+
+# 下载wordpress
+ADD https://wordpress.org/latest.tar.gz /usr/share/nginx/latest.tar.gz
+RUN cd /usr/share/nginx/ && tar xvf latest.tar.gz && rm latest.tar.gz
+RUN mv /usr/share/nginx/html/5* /usr/share/nginx/wordpress
+RUN rm -rf /usr/share/nginx/www
+RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
+RUN chown -R www-data:www-data /usr/share/nginx/www
+
+# Wordpress初始化和启动脚本
+ADD ./start.sh /start.sh
+RUN chmod 755 /start.sh
+
+# private expose
+EXPOSE 3306
+EXPOSE 80
+
+# 为mysql数据库和wordpress安装量
+VOLUME ["/var/lib/mysql", "/usr/share/nginx/www"]
+
+CMD ["/bin/bash", "/start.sh"]
---
+++ start.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# MAINTAINER Eugene Beauty <bybeauby@gmail.com>
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  #mysql必须以这种方式启动，因为它无法从/etc/init.d调用
+  /usr/bin/mysqld_safe &
+  sleep 10s
+  # 这里我们生成随机密码（thank you pwgen!）前两个是mysql用户，最后一批是wp-config.php中的随机密钥
+  WORDPRESS_DB="wordpress"
+  MYSQL_PASSWORD=`pwgen -c -n -1 12`
+  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
+  #This is so the passwords show up in logs.
+  echo mysql root password: $MYSQL_PASSWORD
+  echo wordpress password: $WORDPRESS_PASSWORD
+  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
+
+  sed -e "s/database_name_here/$WORDPRESS_DB/
+  s/username_here/$WORDPRESS_DB/
+  s/password_here/$WORDPRESS_PASSWORD/
+  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+
+  # 下载nginx helper插件
+  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
+  unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
+  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
+
+  # 登录后激活nginx插件
+  cat << ENDL >> /usr/share/nginx/www/wp-config.php
+\$plugins = get_option( 'active_plugins' );
+if ( count( \$plugins ) === 0 ) {
+  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+  foreach ( \$pluginsToActivate as \$plugin ) {
+    if ( !in_array( \$plugin, \$plugins ) ) {
+      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
+    }
+  }
+}
+ENDL
+
+  chown www-data:www-data /usr/share/nginx/www/wp-config.php
+
+  mysqladmin -u root password $MYSQL_PASSWORD
+  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
+  killall mysqld
+fi
+
+# 开始所有的服务
+/usr/local/bin/supervisord -n
+root@web-server01:/home/moon/docker-wordpress-nginx-BAK# 
+root@web-server01:/home/moon/docker-wordpress-nginx-BAK# cat start.sh 
+#!/bin/bash
+# Ubuntu 16.04.4 LTS
+# MAINTAINER Eugene Beauty <bybeauby@gmail.com>
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  #mysql必须以这种方式启动，因为它无法从/etc/init.d调用
+  /usr/bin/mysqld_safe &
+  sleep 10s
+  # 这里我们生成随机密码（谢谢你pwgen！）。 前两个是mysql用户，最后一批是wp-config.php中的随机密钥
+  WORDPRESS_DB="wordpress"
+  MYSQL_PASSWORD=`pwgen -c -n -1 12`
+  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
+  #这是密码在日志中显示的
+  echo mysql root password: $MYSQL_PASSWORD
+  echo wordpress password: $WORDPRESS_PASSWORD
+  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
+
+  sed -e "s/database_name_here/$WORDPRESS_DB/
+  s/username_here/$WORDPRESS_DB/
+  s/password_here/$WORDPRESS_PASSWORD/
+  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+
+  # 下载nginx helper插件
+  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
+  unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
+  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
+
+  # 登录后激活nginx插件
+  cat << ENDL >> /usr/share/nginx/www/wp-config.php
+\$plugins = get_option( 'active_plugins' );
+if ( count( \$plugins ) === 0 ) {
+  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+  foreach ( \$pluginsToActivate as \$plugin ) {
+    if ( !in_array( \$plugin, \$plugins ) ) {
+      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
+    }
+  }
+}
+ENDL
+
+  chown www-data:www-data /usr/share/nginx/www/wp-config.php
+
+  mysqladmin -u root password $MYSQL_PASSWORD
+  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
+  killall mysqld
+fi
+
+# 开始所有的服务
+/usr/local/bin/supervisord -n
+
